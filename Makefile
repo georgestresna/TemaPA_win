a532d932e4d239f2247aa39bb1c467dc88f54d5a
George Stresna <68332128+georgestresna@users.noreply.github.com>
2024-05-16 09:42:29 +0300

    update lab2

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-	gcc main.c Task1.c liste.c Task2.c Task3.c stive.c cozi.c Task4.c arbori.c -o lanParty
-run:
-	./lanParty c.in d.in r.out
-clean:
-	rm -f lanParty
-
-
--- headers/main.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-//#include "Player.h"
-
-Node* Task1(FILE* fin, FILE* fout);
-Node* Task2(FILE* fin, FILE* fout, Node* head);
-Node* Task3(FILE* fout, Node* head);
-void Task4();
